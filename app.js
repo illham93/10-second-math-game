1e3371f2c4696460420e688a36c61c34b303a80a
Devon <devonhamill93@gmail.com>
2024-02-07 16:02:57 -0500

    Basic functionality done

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
 var newQuestion = function() {
-    $('#number1').html(_.random(0,12));
-    $('#number2').html(_.random(0,12));
+    var number1 = _.random(0,12);
+    var number2 = _.random(0,12);
+    var answer = number1 + number2;
+    $('#number1').html(number1);
+    $('#operator').html('+');
+    $('#number2').html(number2);
+    return answer;
 }
 
-var newTimer = function() {
-    var time = 10;
-    $('#timer').html(time);
-    setInterval(function () {
-        if (time > 0) {
-            time--;
-            $('#timer').html(time);
-        }
-    }, 1000);
-
-}
 
 $(document).ready(function() {
-    newQuestion();
-    newTimer();
-    $('#answer').keydown(function() {
 
-    })
+    // Initialize and display the high score
+    var highScore = 0;
+    $('#highScore').html(highScore);
 
-})
+    $('#newGame').click(function() {
+        // Initialize and display the variables
+        var score = 0;
+        var answer = newQuestion();
+        var time = 10;
+        $('#answerBox').prop('disabled', false);
+        $('#answerBox').val('');
+        $('#timer').html(time);
+        $('#score').html(score);
+
+        // Start the countdown
+        var timer = setInterval(function () {
+            if (time > 0) {
+                time--;
+                $('#timer').html(time);
+            } else {
+                clearTimeout(timer);
+                alert('Game over!');
+                if (score > highScore) {
+                    highScore = score;
+                    $('#highScore').html(highScore);
+                }
+                $('#answerBox').prop('disabled', true);
+            }
+        }, 1000);
+
+        // These things happen if the user enters the right answer
+        $('#answerBox').keyup(function() {
+            console.log(answer);
+            if ($('#answerBox').val() == answer) {
+                console.log('correct');
+                time++;
+                score++;
+                $('#score').html(score);
+                $('#answerBox').val('');
+                answer = newQuestion();
+            }
+        });
+    });
+
+});
